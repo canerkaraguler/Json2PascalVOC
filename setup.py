d63cb136be8fcbe3799ac1abae36f80f241121e8
cnrkrglr <caner.karaguler@metltd.net>
2019-06-27 09:16:07 +0300

    Fixed Setup bug

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import setuptools
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
@@ -6,14 +7,15 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
 	name="Json2PascalVoc",
-	version='1.0.2',
+	version='1.0.4',
 	author='Caner Karagüler',
 	author_email='caner.karaguler@gmail.com',
 	long_description = long_description,
 	long_description_content_type="text/markdown",
 	description = 'Package To Convert Json Files To PascalVOC XML files',
 	py_modules=["Converter"],
-	package_dir={'':'Json2PascalVoc'},
+	packages=setuptools.find_packages(),
+	url="https://github.com/canerkaraguler/Json2PascalVOC",
 	classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
